ca918e9e867f6f901fa73a8fee997c0e7309f4b0
DESKTOP-L8O2RPO\Lenovo <abdurrahim.3078@icloud.com>
2021-01-05 12:59:35 +0300

    deploy error

--- src/Redux/Store/configureStore.jsx
+++ src/Redux/Store/configureStore.jsx
@@ -1,13 +1,13 @@
 import {compose,applyMiddleware,createStore,combineReducers} from 'redux';
 import thunk from "redux-thunk";
 import {playListReducer,playListDetailsReducer} from "../Reducers/playListReducers.jsx"
-import { navbarSelectedItemReducer } from "../Reducers/navbarReducers.jsx";
+import { navbarSelectedItemReducer } from "../Reducers/NavbarReducers.jsx";
 import { songReducer } from "../Reducers/songReducers.jsx";
 
 
 const middleware =compose(
     applyMiddleware(thunk)
-    /*, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/
+    , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 const rootReducer =combineReducers({
     playListReducer,
